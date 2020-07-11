61d738ef26c56f0f8e11c44e9ae6773fbacd463e
Bhasin Neeraj <bhasineeraj@MacBook-Pro.local>
2020-07-11 09:55:44 -0400

    Updates

---
+++ precache-manifest.6e6936b2641b12479e8f323273fde9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157cb3d096a832c48878416e13f05692",
+    "url": "/netflixClone/index.html"
+  },
+  {
+    "revision": "6c0ee2367f0e6a96e81f",
+    "url": "/netflixClone/static/css/main.2db576e8.chunk.css"
+  },
+  {
+    "revision": "53271573a054775996cf",
+    "url": "/netflixClone/static/js/2.ad7fb544.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflixClone/static/js/2.ad7fb544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0ee2367f0e6a96e81f",
+    "url": "/netflixClone/static/js/main.d60e9685.chunk.js"
+  },
+  {
+    "revision": "c2ebfca4b867fee391cd",
+    "url": "/netflixClone/static/js/runtime-main.003b2803.js"
+  }
+]);
